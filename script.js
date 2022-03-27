a0f55c1db74dc4f0c93ae0f44c7c6539efef5384
jefersongjr <jeferson_gjr@hotmail.com>
2022-03-27 20:15:59 -0300

    editando texto de acerto

--- script.js
+++ script.js
@@ -1,42 +1,53 @@
-let color1 = `(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`
+const section2 = document.getElementById('colors');
+
+function createColors(ball) {
+  const div = document.createElement('div');
+  div.classList = ball;
+  div.innerHTML = '';
+  div.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 254
+  )}, ${Math.floor(Math.random() * 254)} , ${Math.floor(Math.random() * 254)})`;
+  div.addEventListener('click', clickColor);
+  return div;
+}
+
+function appendColors() {
+  for (let index = 1; index <= 6; index += 1) {
+    section2.appendChild(createColors('ball'));
+  }
+}
+appendColors();
+let i = Math.floor(Math.random() * 5);
+const getCollor = document.querySelectorAll('.ball')[i];
+const getRgb = window.getComputedStyle(getCollor);
+const rgbValue = getRgb.getPropertyValue('background-color');
 
 const section = document.getElementById('text');
-function createP(){
-    const p = document.createElement('p');
-    p.innerHTML = color1;
-    p.id = 'rgb-color';
-    section.appendChild(p);
+function createP() {
+  const p = document.createElement('p');
+  p.innerHTML = `${rgbValue}`;
+  p.id = 'rgb-color';
+  section.appendChild(p);
 }
 createP();
 
-const section2 = document.getElementById('colors');
-
-function createColors(ball){
-    const div = document.createElement('div');
-    div.classList = ball;
-    div.innerHTML = ''
-    return div;
+function createAnswer(x) {
+  const answer = document.createElement('h2');
+  answer.id = 'answer';
+  answer.innerText = x;
+  section2.appendChild(answer);
+  return answer;
 }
 
-function appendColors(){
-    for(let index = 1 ; index <= 6; index += 1){
-        section2.appendChild(createColors('ball'))
-    }
+createAnswer('Escolha uma cor');
+
+const h2 = document.getElementById('answer');
+function clickColor(event) {
+  let click = window.getComputedStyle(event.target);
+  let click2 = click.getPropertyValue('background-color');
+  if (click2 === rgbValue) {
+    h2.innerText = 'Acertou';
+  } else if (click2 !== rgbValue) {
+    h2.innerText = 'Errou';
+  }
 }
-appendColors();
-
-const balls = document.querySelectorAll('.ball');
- 
-const ball1 = document.querySelectorAll('.ball')[0];
-const ball2 = document.querySelectorAll('.ball')[1];
-const ball3 = document.querySelectorAll('.ball')[2];
-const ball4 = document.querySelectorAll('.ball')[3];
-const ball5 = document.querySelectorAll('.ball')[4];
-const ball6 = document.querySelectorAll('.ball')[5];
-
-ball1.style.backgroundColor =  `rgb(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`;
-ball2.style.backgroundColor = `rgb(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`;;
-ball3.style.backgroundColor = `rgb(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`;;
-ball4.style.backgroundColor = `rgb(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`;;
-ball5.style.backgroundColor =`rgb(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`;
-ball6.style.backgroundColor = `rgb(${ Math.floor(Math.random() * 254)}, ${ Math.floor(Math.random() * 254)} , ${ Math.floor(Math.random() * 254)})`;
