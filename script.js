4ac954dd4ebd56a9c4632d00c8371183b03c654e
jefersongjr <jeferson_gjr@hotmail.com>
2022-03-27 22:24:12 -0300

    estilizando o projeto

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const section2 = document.getElementById('colors');
 
-function createColors(ball, color) {
+function createColors(ball) {
   const div = document.createElement('div');
   div.classList = ball;
   div.innerHTML = '';
-  div.style.backgroundColor = color;
+  div.style.backgroundColor = `rgb(${Math.floor(
+    Math.random() * 254
+  )}, ${Math.floor(Math.random() * 254)},${Math.floor(Math.random() * 254)})`;
   div.addEventListener('click', clickColor);
   return div;
 }
 
 function appendColors() {
   for (let index = 1; index <= 6; index += 1) {
-    section2.appendChild(
-      createColors(
-        'ball',
-        `rgb(${Math.floor(Math.random() * 254)}, ${Math.floor(
-          Math.random() * 254
-        )}, ${Math.floor(Math.random() * 254)})`
-      )
-    );
+    section2.appendChild(createColors('ball'));
   }
 }
 appendColors();
@@ -65,7 +60,7 @@ const btnReset = document.getElementById('reset-game');
 function resetGame() {
   const getCollor = document.querySelectorAll('.ball');
   h2.innerText = 'Escolha uma cor';
-  for (let index = 0; index <= 6; index += 1) {
+  for (let index = 0; index < getCollor.length; index += 1) {
     getCollor[index].style.backgroundColor = `rgb(${Math.floor(
       Math.random() * 254
     )}, ${Math.floor(Math.random() * 254)}, ${Math.floor(
